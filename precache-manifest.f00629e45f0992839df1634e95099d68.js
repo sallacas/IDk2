bdc21f5f8696166b924c6d52b7e1b2bbe60588bf
Sallacas <jdcasallas12@gmail.com>
2021-03-08 21:14:51 -0500

    Updates

---
+++ precache-manifest.f00629e45f0992839df1634e95099d68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c72f831c51e87c83da5c83821cd1913",
+    "url": "/IDk2/index.html"
+  },
+  {
+    "revision": "ff5b3ca21f585397b917",
+    "url": "/IDk2/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/IDk2/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/IDk2/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff5b3ca21f585397b917",
+    "url": "/IDk2/static/js/main.a74c713f.chunk.js"
+  },
+  {
+    "revision": "51605692bde9b9c851bf",
+    "url": "/IDk2/static/js/runtime-main.2b372b24.js"
+  }
+]);
